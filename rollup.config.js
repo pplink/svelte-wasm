c659e9238057007473dd7602ebe07375de5c8d53
Jaeseong Seo <07js23@gmail.com>
2021-09-23 21:11:50 +0900

    Configure wasm plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import rust from '@wasm-tool/rollup-plugin-rust';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -64,6 +65,10 @@ export default {
 			sourceMap: !production,
 			inlineSources: !production
 		}),
+    rust({
+      inlineWasm: true,
+      watchPatterns: ['src/**/*.rs'],
+    }),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
