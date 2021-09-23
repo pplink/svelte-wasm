5161c4ffa8fe6448d5aae4cea82a2a61468d8806
Jaeseong Seo <07js23@gmail.com>
2021-09-23 21:28:52 +0900

    Add fibonacci

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 	export let name: string;
+  const wasmExports = import('../Cargo.toml').then((wasm) => wasm.default());
   function wasmGreet() {
-    import('../Cargo.toml').then((wasm) => wasm.default()).then((exports) => {
+    wasmExports.then((exports) => {
       exports.greet('PPLINK');
     })
   }
+
+  const fibonacciPromise = wasmExports.then((wasm) => wasm.fibonacci);
+
+  let fibIndex = 1;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
   <button on:click={wasmGreet}>Greet</button>
+  {#await fibonacciPromise then fibonacci}
+    <ol>
+      {#each Array(fibIndex).fill(null) as _, index}
+        <li>{ fibonacci(index + 1) }</li>
+      {/each}
+    </ol>
+    <button on:click={() => fibIndex += 1}>Show next</button>
+  {/await}
 </main>
 
 <style>
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,12 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+#[wasm_bindgen]
+pub fn fibonacci(num: i32) -> i32 {
+    if num <= 1 {
+        num
+    } else {
+        fibonacci(num - 1) + fibonacci(num - 2)
+    }
+}
