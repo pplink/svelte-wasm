3261a96ac8183a8a5a9305f058db91b8d5b26299
Jaeseong Seo <07js23@gmail.com>
2021-09-23 21:30:47 +0900

    Add dom manipulation example

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,19 @@ pub fn fibonacci(num: i32) -> i32 {
         fibonacci(num - 1) + fibonacci(num - 2)
     }
 }
+
+// Called by our JS entry point to run the example
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello from Rust!"));
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
